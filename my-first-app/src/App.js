d5cc4488a4c100ffc7bd69629388b42c64f28f33
Iliya Nedelchev <il.nedelchev@gmail.com>
2021-05-28 17:35:56 +0300

    adding new component

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,32 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
+import Person from './Person/Person';
 
 class App extends Component {
-  // return (
-  //   // <div className="App">
-  //   //   <header className="App-header">
-  //   //     <img src={logo} className="App-logo" alt="logo" />
-  //   //     <p>
-  //   //       Edit <code>src/App.js</code> and save to reload.
-  //   //     </p>
-  //   //     <a
-  //   //       className="App-link"
-  //   //       href="https://reactjs.org"
-  //   //       target="_blank"
-  //   //       rel="noopener noreferrer"
-  //   //     >
-  //   //       Learn React
-  //   //     </a>
-  //   //   </header>
-  //   // </div>
-
-    
-  // );
-
-  render(){
-    return React.createElement('div', { className:'App' }, React.createElement('h1', null, 'I\'m robot!!'));
+  render() {
+    return (
+      <div className="App">
+        <Person />
+      </div>
+    );
   }
+
+  // render(){
+  //   return React.createElement('div', { className:'App' }, React.createElement('h1', null, 'I\'m robot!!'));
+  // }
 }
 
 export default App;
